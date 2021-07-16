118df73aa018ba34514c0e20f536790f91cae2fb
LucasLacer <37111083+LucasLacer@users.noreply.github.com>
2021-07-16 15:00:37 -0300

    Update challenge

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,36 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle) {
+      const task = {
+        id: Math.random(),
+        title: newTaskTitle,
+        isComplete: false,
+      }
+
+      setTasks([...tasks, task])
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const taskList = tasks.map(task => {
+      if (task.id === id) {
+        task.isComplete = !task.isComplete
+      }
+      return task
+    })
+    if (taskList) {
+      setTasks(taskList)
+    }
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const taskList = tasks.filter(task => task.id !== id)
+    if (taskList) {
+      setTasks(taskList)
+    }
   }
 
   return (
